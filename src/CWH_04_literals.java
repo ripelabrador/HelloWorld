5512d43aa26ad19608858edab0b7888653ef95e2
jack <shivam825289@gmail.com>
2023-07-16 12:11:51 +0530

    commit 2

---
+++ src/CWH_04_literals.java
@@ -0,0 +1,9 @@
+public class CWH_04_literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        System.out.println(age);
+    }
+}
--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,9 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.print("Hello World");
+        int num1 = 6;
+        int num2 = 8;
+        int num3 = 7;
+        System.out.println();
     }
 }
